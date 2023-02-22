d8afc675fdcb106e5016b04982dfc372f284abc2
Guga <gustavo.ferreira0821@gmail.com>
2023-02-22 20:39:54 -0300

    :sparkles: feat: dele user by id

--- routes/users.js
+++ routes/users.js
@@ -61,6 +61,17 @@ module.exports = (app)=>{
         });
 
     });
+
+    routeId.delete((req, res)=>{
+
+        db.remove({_id: req.params.id}, {}, err=>{
+            if(err){
+                app.utils.error.send(err,req, res);
+            }else{
+                res.status(200).json(req.params);
+            }
+        });
+    })
     
 }/*as consing said, this file will be put into the app,
    so, the app "will come here" to get it.
