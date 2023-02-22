ef23a48264d8e435dad61dc25862f1f537751926
Guga <gustavo.ferreira0821@gmail.com>
2023-02-22 15:22:13 -0300

    :sparkles: feat:  inserting data in nedb

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename:'users.db',
+    autoload:true,//create the file if dosn't exist
+});
+
 module.exports = (app)=>{
 
     app.get('/users', (req, res)=>{
@@ -14,8 +20,16 @@ module.exports = (app)=>{
     
     
     app.post('/users', (req, res)=>{
-        console.log(req.body);
-        res.json(req.body);
+        
+
+        db.insert(req.body, (err, user)=>{
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json(user);
+            }else{
+                res.status(200).json(user);
+            }
+        });
 
     });
     
