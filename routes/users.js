9a84427db07a0e42950851152e9e1ca2b333f0d5
Guga <gustavo.ferreira0821@gmail.com>
2023-02-22 15:39:35 -0300

    :sparkles: feat: list all users in nddb

--- routes/users.js
+++ routes/users.js
@@ -2,30 +2,40 @@ let NeDB = require('nedb');
 let db = new NeDB({
     filename:'users.db',
     autoload:true,//create the file if dosn't exist
-});
+});//é uma instância do db exatamente feita para o arquivo de users
 
 module.exports = (app)=>{
 
+
     app.get('/users', (req, res)=>{
-        res.statusCode= 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users: [{
-                name:'Gustavo',
-                email:'gustavo.silva@gmail.com',
-                id:1
-            }]
+
+        db.find({}).sort({name:1}).exec((err, users)=>{
+        
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error:err,
+                });
+            }else{
+    
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+    
+                res.status(200).json({users})
+            }
+    
         });
     });
-    
+
     
     app.post('/users', (req, res)=>{
         
 
         db.insert(req.body, (err, user)=>{
             if(err){
-                console.log(`error: ${err}`);
-                res.status(400).json(user);
+                res.status(400).json({
+                    err:err,
+                });
             }else{
                 res.status(200).json(user);
             }
