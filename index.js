348dac72851e3e5773658d71657b4b99869e041d
Gustavo Silva <gustavo.silva@ear.local>
2023-03-01 06:55:56 -0300

    :wrench: chore: validation to user post

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const consign = require('consign');
 
 let app = express();
- 
+
 app.use(express.json());
 
 consign().include('utils').include('routes').into(app);//put all files from routes folder into app
 
-app.listen(3000, '127.0.0.1', ()=>{
+app.listen(4000, '127.0.0.1', () => {
     console.log('servidor rodando!');
 })
--- utils/validator.js
+++ utils/validator.js
@@ -1,18 +1,19 @@
-const {check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 module.exports = {
 
-    checkFields:()=>{
+    checkFields: () => {
         return [
-        check('username', 'invalid email').isEmail(),
-        check('password', 'min passwords characteres: 5').isLength({min:5}),
-    ]},
+            check('_name', 'invalid name').isLength({ min: 3 }),
+            check('_password', 'min passwords characteres: 5').isLength({ min: 5 }),
+        ]
+    },
 
-    reportFieldsValidation:(app, req, res)=>{
+    reportFieldsValidation: (app, req, res) => {
         const errors = validationResult(req);
-        if(!errors.isEmpty()){
+        if (!errors.isEmpty()) {
             return app.utils.error.send(errors, req, res);
-        }else{
+        } else {
             return true
         }
     }
