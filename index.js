e2224265fdf724d1c8cf8299667a7ba4476d79d7
Gustavo Silva <gustavo.silva@ear.local>
2023-03-02 12:51:21 -0300

    :wrench: chore: request limit 50mb

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const consign = require('consign');
 
 let app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 consign().include('utils').include('routes').into(app);//put all files from routes folder into app
 
-app.listen(4000, '127.0.0.1', ()=>{
+app.listen(4000, '127.0.0.1', () => {
 
     console.log('servidor rodando!');
 })
